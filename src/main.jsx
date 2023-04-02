caaf7a54f42d8bea2ec92bccbb7b98a5e7f48a44
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-04-02 14:17:02 +0200

    File name changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import './Styles/index.scss'
+import './Styles/Index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
