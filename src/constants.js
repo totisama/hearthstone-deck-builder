86a1e1e39d76f8900acf852057c604e1155f12c2
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-04-10 19:24:47 +0200

    Change/add constants

--- src/constants.js
+++ src/constants.js
@@ -1,77 +1,73 @@
 export const MANA_COSTS = [
-  { value: 0, label: '0' },
-  { value: 1, label: '1' },
-  { value: 2, label: '2' },
-  { value: 3, label: '3' },
-  { value: 4, label: '4' },
-  { value: 5, label: '5' },
-  { value: 6, label: '6' },
-  { value: 7, label: '7' },
-  { value: 8, label: '8' },
-  { value: 9, label: '9' },
-  { value: 10, label: '10+' }
+  { slug: 0, label: '0' },
+  { slug: 1, label: '1' },
+  { slug: 2, label: '2' },
+  { slug: 3, label: '3' },
+  { slug: 4, label: '4' },
+  { slug: 5, label: '5' },
+  { slug: 6, label: '6' },
+  { slug: 7, label: '7' },
+  { slug: 8, label: '8' },
+  { slug: 9, label: '9' },
+  { slug: 10, label: '10+' }
 ]
 
 export const ATTACK_VALUES = [
-  { value: '', label: 'Any Attack' },
-  { value: 0, label: 'Attack: 0' },
-  { value: 1, label: 'Attack: 1' },
-  { value: 2, label: 'Attack: 2' },
-  { value: 3, label: 'Attack: 3' },
-  { value: 4, label: 'Attack: 4' },
-  { value: 5, label: 'Attack: 5' },
-  { value: 6, label: 'Attack: 6' },
-  { value: 7, label: 'Attack: 7' },
-  { value: 8, label: 'Attack: 8' },
-  { value: 9, label: 'Attack: 9' },
-  { value: 10, label: 'Attack: 10+' }
+  { slug: '', label: 'Any Attack' },
+  { slug: 0, label: 'Attack: 0' },
+  { slug: 1, label: 'Attack: 1' },
+  { slug: 2, label: 'Attack: 2' },
+  { slug: 3, label: 'Attack: 3' },
+  { slug: 4, label: 'Attack: 4' },
+  { slug: 5, label: 'Attack: 5' },
+  { slug: 6, label: 'Attack: 6' },
+  { slug: 7, label: 'Attack: 7' },
+  { slug: 8, label: 'Attack: 8' },
+  { slug: 9, label: 'Attack: 9' },
+  { slug: 10, label: 'Attack: 10+' }
 ]
 
 export const HEALTH_VALUES = [
-  { value: '', label: 'Any Health' },
-  { value: 1, label: 'Health: 1' },
-  { value: 2, label: 'Health: 2' },
-  { value: 3, label: 'Health: 3' },
-  { value: 4, label: 'Health: 4' },
-  { value: 5, label: 'Health: 5' },
-  { value: 6, label: 'Health: 6' },
-  { value: 7, label: 'Health: 7' },
-  { value: 8, label: 'Health: 8' },
-  { value: 9, label: 'Health: 9' },
-  { value: 10, label: 'Health: 10+' }
-]
-
-// WIP: Request from API
-export const RARITIES = [
-  { value: '', label: 'Any Rarity' },
-  { value: 'common', label: 'Common' },
-  { value: 'free', label: 'Free' },
-  { value: 'rare', label: 'Rare' },
-  { value: 'epic', label: 'Epic' },
-  { value: 'legendary', label: 'Legendary' }
+  { slug: '', label: 'Any Health' },
+  { slug: 1, label: 'Health: 1' },
+  { slug: 2, label: 'Health: 2' },
+  { slug: 3, label: 'Health: 3' },
+  { slug: 4, label: 'Health: 4' },
+  { slug: 5, label: 'Health: 5' },
+  { slug: 6, label: 'Health: 6' },
+  { slug: 7, label: 'Health: 7' },
+  { slug: 8, label: 'Health: 8' },
+  { slug: 9, label: 'Health: 9' },
+  { slug: 10, label: 'Health: 10+' }
 ]
 
 export const MANA_COSTS_SELECT = [
-  { value: '', label: 'Mana' },
-  { value: 0, label: 'Mana: 0' },
-  { value: 1, label: 'Mana: 1' },
-  { value: 2, label: 'Mana: 2' },
-  { value: 3, label: 'Mana: 3' },
-  { value: 4, label: 'Mana: 4' },
-  { value: 5, label: 'Mana: 5' },
-  { value: 6, label: 'Mana: 6' },
-  { value: 7, label: 'Mana: 7' },
-  { value: 8, label: 'Mana: 8' },
-  { value: 9, label: 'Mana: 9' },
-  { value: 10, label: 'Mana: 10+' }
+  { slug: '', label: 'Mana' },
+  { slug: 0, label: 'Mana: 0' },
+  { slug: 1, label: 'Mana: 1' },
+  { slug: 2, label: 'Mana: 2' },
+  { slug: 3, label: 'Mana: 3' },
+  { slug: 4, label: 'Mana: 4' },
+  { slug: 5, label: 'Mana: 5' },
+  { slug: 6, label: 'Mana: 6' },
+  { slug: 7, label: 'Mana: 7' },
+  { slug: 8, label: 'Mana: 8' },
+  { slug: 9, label: 'Mana: 9' },
+  { slug: 10, label: 'Mana: 10+' }
 ]
 
 export const API_HOST = 'https://us.api.blizzard.com/hearthstone'
 
 export const DEFAULT_LOCALE = 'en_US'
 
-export const XS = 256
-export const SM = 512
-export const MD = 768
-export const LG = 1024
-export const XL = 1280
+export const XS_VALUE = 256
+export const SM_VALUE = 512
+export const MD_VALUE = 768
+export const LG_VALUE = 1024
+export const XL_VALUE = 1280
+
+export const EXTRA_SMALL = 'XS'
+export const SMALL = 'SM'
+export const MEDIUM = 'MD'
+export const LARGE = 'LG'
+export const EXTRA_LARGE = 'XL'
