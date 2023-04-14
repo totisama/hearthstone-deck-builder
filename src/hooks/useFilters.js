0d5d665b6c53b670780f8fbb49e7b9c814ba6a68
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-04-14 20:44:30 +0200

    Adding function to generate query params and returning new values

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -2,9 +2,35 @@ import { useContext } from 'react'
 import { FiltersContext } from '../Context/Filters'
 
 export const useFilters = () => {
-  const { filters, setFilters, showSubFilters, setShowSubFilters } = useContext(FiltersContext)
+  const {
+    filters,
+    setFilters,
+    showSubFilters,
+    setShowSubFilters,
+    page,
+    setPage
+  } = useContext(FiltersContext)
+
+  const generateQueryParams = () => {
+    const queryParams = new URLSearchParams({})
+    const entries = Object.entries(filters)
+
+    entries.forEach(entry => {
+      if (entry[1] !== '') {
+        queryParams.append(entry[0], entry[1])
+      }
+    })
+
+    return queryParams.toString()
+  }
 
   return {
-    filters, setFilters, showSubFilters, setShowSubFilters
+    filters,
+    setFilters,
+    showSubFilters,
+    setShowSubFilters,
+    generateQueryParams,
+    page,
+    setPage
   }
 }
