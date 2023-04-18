aa6ed42a88d155dfa2b4d02b9b088636ea790d58
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-04-18 14:09:21 +0200

    WIP: Finish styling status bar

--- src/Components/StatusBar.jsx
+++ src/Components/StatusBar.jsx
@@ -1,9 +1,26 @@
 import '../Styles/StatusBar.scss'
+import { useFilters } from '../hooks/useFilters'
+import { useStatusBar } from '../hooks/useStatusBar'
 
 const StatusBar = () => {
+  const { status } = useStatusBar()
+  const { removeFilter } = useFilters()
+  const {
+    cardCount,
+    statusFilters,
+    set
+  } = status
+
   return (
-    <section>
-      <h1>StatusBar</h1>
+    <section className='statusBar'>
+      <h1>{cardCount} cards found for "{set}"</h1>
+      <div className='removableFilters'>
+        {Object.entries(statusFilters).map(([key, value]) => (
+          <button key={value} onClick={() => { removeFilter(key) }}>{value}
+            <span>✕</span>
+          </button>
+        ))}
+      </div>
     </section>
   )
 }
