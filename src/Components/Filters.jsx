9216b9583002c1e70f47d6bec06c83476d1fa565
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-05-01 21:11:40 +0200

    Adding conditions to not show tags depending on prop

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -6,7 +6,7 @@ import { useMetadata } from '../hooks/useMetadata'
 import { useFilters } from '../hooks/useFilters'
 import { useDebounce } from '../hooks/useDebounce'
 
-const Filters = () => {
+const Filters = ({ deckBuilder = false }) => {
   const { setFilters, showSubFilters, setShowSubFilters, filters } = useFilters()
   const { metadata } = useMetadata()
   const [search, setSearch] = useState('')
@@ -41,19 +41,32 @@ const Filters = () => {
     <section className='filters'>
       <div className='mainFilters'>
         <div className='inputFilters'>
-          <select className='hidden' value={filters.set} onChange={handleOnChangeSet}>
-            <option key={1} value='standard'>Standard</option>
-            <option key={2} value='wild'>Wild Cards</option>
-            {sets.map((set) => (
-              <option key={set.id} value={set.slug}>{set.name}</option>
-            ))}
-          </select>
-          <select className='hidden' value={filters.class} onChange={handleOnChangeClass}>
-            <option key={0} value=''>All Classes</option>
-            {classes.map((heroClass) => (
-              <option key={heroClass.id} value={heroClass.slug}>{heroClass.name}</option>
-            ))}
-          </select>
+          {/* {deckBuilder
+            ? (
+              <h1>deckBuilder</h1>
+              )
+            : null} */}
+          {!deckBuilder
+            ? (
+              <select className='hidden' value={filters.set} onChange={handleOnChangeSet}>
+                <option key={1} value='standard'>Standard</option>
+                <option key={2} value='wild'>Wild Cards</option>
+                {sets.map((set) => (
+                  <option key={set.id} value={set.slug}>{set.name}</option>
+                ))}
+              </select>
+              )
+            : null}
+          {!deckBuilder
+            ? (
+              <select className='hidden' value={filters.class} onChange={handleOnChangeClass}>
+                <option key={0} value=''>All Classes</option>
+                {classes.map((heroClass) => (
+                  <option key={heroClass.id} value={heroClass.slug}>{heroClass.name}</option>
+                ))}
+              </select>
+              )
+            : null}
           <ManaFilter />
           <input type='text' value={filters.textFilter} placeholder='Search' onChange={(event) => setSearch(event.target.value)} />
         </div>
