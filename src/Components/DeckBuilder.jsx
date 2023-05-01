eb9dcc7e7c6c5d41cc24dd0a00c8efbb53cb3ef2
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-05-01 21:10:28 +0200

    Adding general style and basic functionality to add cards to deck

--- src/Components/DeckBuilder.jsx
+++ src/Components/DeckBuilder.jsx
@@ -1,30 +1,98 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import '../Styles/DeckBuilder.scss'
 import { useParams, useNavigate } from 'react-router-dom'
-import { HEROS_LIST } from '../constants'
-// import NoCards from './NoCards'
+import { HEROS_LIST, HEROS_NAME } from '../constants'
+import NoCards from './NoCards'
 import Filters from './Filters'
 import StatusBar from './StatusBar'
+import { useCards } from '../hooks/useCards'
+import { InView } from 'react-intersection-observer'
+import Loader from './Loader'
 
 const DeckBuilder = () => {
   const { hero } = useParams()
   const navigate = useNavigate()
+  const { cards, getNewCards } = useCards()
+  const cardsEntries = Object.entries(cards)
+  const [deck, setDeck] = useState([])
+  const [cardsCount, setCardsCount] = useState(0)
+  const [deckSize] = useState(30)
+  const [loading, setLoading] = useState(true)
+
+  const addCard = (card) => {
+    // WIP: add condition if Renathal is added
+    if (cardsCount >= deckSize) {
+      return
+    }
+
+    const newDeck = [...deck]
+
+    // WIP: increment quantity if card already in deck
+    card.quantity = 1
+
+    newDeck.push(card)
+
+    setDeck(newDeck)
+    setCardsCount(prevState => prevState + 1)
+  }
 
   useEffect(() => {
     if (!HEROS_LIST.includes(hero)) {
       navigate('/deckbuilder')
+
+      return
     }
+
+    setLoading(false)
   }, [])
 
-  return (
-    <>
-      <Filters deckBuilder />
-      <StatusBar />
-      <main>
-        DeckBuilder
-      </main>
-    </>
-  )
+  return !loading
+    ? (
+      <>
+        <Filters deckBuilder />
+        <main className='deckBuilderContainer'>
+          <div className='heroCardsContainer'>
+            <StatusBar />
+            {cardsEntries.length > 0
+              ? (
+                <>
+                  {cardsEntries.map(([value, cards]) => (
+                    <div key={value}>
+                      <h2 className='title'>{value}</h2>
+                      <div className='cards'>
+                        {cards.map((card) => (
+                          <div key={card.id} onClick={() => addCard(card)}>
+                            <img className='image' src={card.image} alt={card.name} />
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  ))}
+                  <InView as='div' onChange={(inView) => { getNewCards(inView) }} />
+                </>)
+              : <NoCards />}
+          </div>
+          <div className='deckContainer'>
+            <div className='topContainer'>
+              <div className='top'>
+                <div className='topBorder' />
+                <div className='titleContainer'>
+                  <h3 className='heroTitle'>Standard {HEROS_NAME[hero]} Deck</h3>
+                  <h3 className='cardsCount'>{cardsCount} / {deckSize}</h3>
+                </div>
+                <div style={{ backgroundImage: `url(../src/assets/classesBackground/${hero}.jpg)` }} className='heroBackground' />
+              </div>
+            </div>
+            <div className='deck'>
+              {deck.map((card) => (
+                <img key={card.id} className='image' src={card.cropImage} alt={card.name} />
+              ))}
+            </div>
+            <div className='deckBottom' />
+          </div>
+        </main>
+      </>)
+    : <Loader />
 }
 
 export default DeckBuilder
