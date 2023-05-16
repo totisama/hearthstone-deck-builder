88eb6f635973e0fcee94ff5e59996822c9a35f9a
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-05-16 16:56:48 +0200

    Add logic to display the current runes
    When adding or removing a card update the current runes depending on the cards on the deck

--- src/Components/DeckBuilder.jsx
+++ src/Components/DeckBuilder.jsx
@@ -20,6 +20,7 @@ const DeckBuilder = () => {
   const [addedCardsAmount, setAddedCardsAmount] = useState({})
   const [deckSize, setDeckSize] = useState(30)
   const [currentRunes, setCurrentRunes] = useState({ blood: 0, frost: 0, unholy: 0 })
+  const [currentRunesName, setCurrentRunesName] = useState(['', '', ''])
   const [isDeathKnigth, setIsDeathKnigth] = useState(false)
   const [loading, setLoading] = useState(true)
 
@@ -54,17 +55,7 @@ const DeckBuilder = () => {
         return
       }
 
-      const currentRunesCopy = { ...currentRunes }
-
-      for (const [name, value] of Object.entries(runeCost)) {
-        if (value === 0 || value < currentRunesCopy[name]) {
-          continue
-        }
-
-        currentRunesCopy[name] = value
-      }
-
-      setCurrentRunes(currentRunesCopy)
+      updateCurrentRunes(runeCost)
     }
 
     const deckCopy = [...deck]
@@ -79,6 +70,32 @@ const DeckBuilder = () => {
 
     setAddedCardsAmount(addedCardsAmountCopy)
     setDeck(deckCopy)
+  }
+
+  const updateCurrentRunes = (runeCost) => {
+    const currentRunesCopy = { ...currentRunes }
+    let currentRunesNameCopy = [...currentRunesName]
+
+    for (const [name, value] of Object.entries(runeCost)) {
+      if (value === 0 || value <= currentRunesCopy[name]) {
+        continue
+      }
+
+      if (getRunesAmount() < 3) {
+        for (let index = 0; index < value - currentRunes[name]; index++) {
+          const emptyIndex = currentRunesNameCopy.findIndex((element) => element === '')
+
+          if (emptyIndex >= 0) {
+            currentRunesNameCopy = currentRunesNameCopy.with(emptyIndex, name).sort()
+          }
+        }
+      }
+
+      currentRunesCopy[name] = value
+    }
+
+    setCurrentRunesName(currentRunesNameCopy)
+    setCurrentRunes(currentRunesCopy)
   }
 
   const compareIncomingRunes = (incomingRunes) => {
@@ -131,6 +148,11 @@ const DeckBuilder = () => {
       setDeckSize(30)
     }
 
+    const runeCost = card.runeCost
+    if (isDeathKnigth && runeCost) {
+      removeRunesFromcard(runeCost, cardId)
+    }
+
     const addedCardsAmountCopy = { ...addedCardsAmount }
     let deckCopy = [...deck]
 
@@ -142,6 +164,98 @@ const DeckBuilder = () => {
 
     setAddedCardsAmount(addedCardsAmountCopy)
     setDeck(deckCopy)
+  }
+
+  const removeRunesFromcard = (runeCost, cardId) => {
+    const localRunes = { blood: 0, frost: 0, unholy: 0 }
+
+    if (getDeckSize() === 1) {
+      setCurrentRunesName(['', '', ''])
+      setCurrentRunes(localRunes)
+
+      return
+    }
+
+    // Find the highest rune for each type left on the deck
+    for (const card of deck) {
+      // WIP: Solve problem when same card id is left on deck
+      if (!card.runeCost || (card.id === cardId && addedCardsAmount[cardId] === 1)) {
+        continue
+      }
+
+      if (card.runeCost.blood > localRunes.blood) {
+        localRunes.blood = card.runeCost.blood
+      }
+      if (card.runeCost.frost > localRunes.frost) {
+        localRunes.frost = card.runeCost.frost
+      }
+      if (card.runeCost.unholy > localRunes.unholy) {
+        localRunes.unholy = card.runeCost.unholy
+      }
+    }
+
+    // Conditions to remove the amount of runes for each type depending on the highest rune left
+    let currentRunesNameCopy = [...currentRunesName]
+
+    // Blood conditions
+    if (localRunes.blood < runeCost.blood) {
+      if (localRunes.blood > 0) {
+        for (let index = 0; index < runeCost.blood - localRunes.blood; index++) {
+          const bloodIndex = currentRunesNameCopy.findIndex((element) => element === 'blood')
+
+          currentRunesNameCopy = currentRunesNameCopy.with(bloodIndex, '')
+        }
+      } else {
+        const bloodAmount = currentRunesNameCopy.filter((element) => element === 'blood').length
+
+        for (let index = 0; index < bloodAmount; index++) {
+          const bloodIndex = currentRunesNameCopy.findIndex((element) => element === 'blood')
+
+          currentRunesNameCopy = currentRunesNameCopy.with(bloodIndex, '')
+        }
+      }
+    }
+
+    // Frost conditions
+    if (localRunes.frost < runeCost.frost) {
+      if (localRunes.frost > 0) {
+        for (let index = 0; index < runeCost.frost - localRunes.frost; index++) {
+          const frostIndex = currentRunesNameCopy.findIndex((element) => element === 'frost')
+
+          currentRunesNameCopy = currentRunesNameCopy.with(frostIndex, '')
+        }
+      } else {
+        const frostAmount = currentRunesNameCopy.filter((element) => element === 'frost').length
+
+        for (let index = 0; index < frostAmount; index++) {
+          const frostIndex = currentRunesNameCopy.findIndex((element) => element === 'frost')
+
+          currentRunesNameCopy = currentRunesNameCopy.with(frostIndex, '')
+        }
+      }
+    }
+
+    // Unholy conditions
+    if (localRunes.unholy < runeCost.unholy) {
+      if (localRunes.unholy > 0) {
+        for (let index = 0; index < runeCost.unholy - localRunes.unholy; index++) {
+          const unholyIndex = currentRunesNameCopy.findIndex((element) => element === 'unholy')
+
+          currentRunesNameCopy = currentRunesNameCopy.with(unholyIndex, '')
+        }
+      } else {
+        const unholyAmount = currentRunesNameCopy.filter((element) => element === 'unholy').length
+
+        for (let index = 0; index < unholyAmount; index++) {
+          const unholyIndex = currentRunesNameCopy.findIndex((element) => element === 'unholy')
+
+          currentRunesNameCopy = currentRunesNameCopy.with(unholyIndex, '')
+        }
+      }
+    }
+
+    setCurrentRunes(localRunes)
+    setCurrentRunesName(currentRunesNameCopy)
   }
 
   const getDeckSize = () => {
@@ -164,6 +278,12 @@ const DeckBuilder = () => {
     return classes
   }
 
+  const getRuneClass = (value, index) => {
+    const className = `rune ${value}`
+
+    return className
+  }
+
   const cardAddedFilter = (id, rarityId, runeCost) => {
     let className = ''
 
@@ -172,6 +292,35 @@ const DeckBuilder = () => {
     ) {
       className = 'amountLimit'
     }
+
+    // if (isDeathKnigth && runeCost) {
+    //   const amount = getRunesAmount()
+
+    //   if (amount === 0) {
+    //     return className
+    //   }
+
+    //   console.log('-----------------------------------------------')
+    //   for (const [name, value] of Object.entries(runeCost)) {
+    //     if (value === 0) {
+    //       continue
+    //     }
+
+    //     if (amount + value > 3) {
+    //       className = 'runeAmountLimit'
+    //     }
+
+    //     console.log(name, value)
+    //     // const runesSize = getRunesAmount()
+    //     // const bloodValue = runeCost.blood
+    //     // const frostValue = runeCost.frost
+    //     // const unholyValue = runeCost.unholy
+
+    //     // if (runesSize === 1) {
+
+    //     // }
+    //   }
+    // }
 
     return className
   }
@@ -239,6 +388,16 @@ const DeckBuilder = () => {
                 <div style={{ backgroundImage: `url(../src/assets/classesBackground/${hero}.jpg)` }} className='heroBackground' />
               </div>
             </div>
+            {isDeathKnigth
+              ? (
+                <div className='runesContainer'>
+                  <div className='runes'>
+                    {currentRunesName.map((value, index) =>
+                      <div key={index} className={getRuneClass(value, index)} />
+                    )}
+                  </div>
+                </div>)
+              : null}
             <div className='deck'>
               {deck.map((card) => (
                 <div key={card.id} className='deckCardContainer'>
