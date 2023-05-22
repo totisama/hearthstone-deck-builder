f543f6636ea6982eeb0724644375b55648b3e529
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-05-22 13:18:58 +0200

    Add new CardPack Component

---
+++ src/Components/CardPack.jsx
@@ -0,0 +1,19 @@
+import { useParams } from 'react-router-dom'
+import { useDeck } from '../hooks/useDeck'
+
+const CardPack = ({ setDeckOpen }) => {
+  const { hero } = useParams()
+  const { deckSize, currentDeckSize } = useDeck()
+
+  return (
+    <div className='cardPackContainer' onClick={() => { setDeckOpen(true) }}>
+      <div className='cardPack' />
+      <div className='cardPackIcon' style={{ backgroundImage: `url(../src/assets/deckHamburger/${hero}Icon.png)` }} />
+      <div className='deckSizeValues'>
+        {currentDeckSize} / {deckSize}
+      </div>
+    </div>
+  )
+}
+
+export default CardPack
